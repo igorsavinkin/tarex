cefefb7adbe40f6532cdb8403dd2484d57c1c3cf
Igor Savinkin <igor.savinkin@gmail.com>
2014-11-26 16:15:52 +0300

    немного больше продвунулись с посылкой файлов как вложений, using PHPMailer

--- htdocs/protected/controllers/PriceListSettingController.php
+++ htdocs/protected/controllers/PriceListSettingController.php
@@ -144,64 +144,11 @@ class PriceListSettingController extends Controller
 		foreach(PriceListSetting::model()->findAll($criteria) as $pls)
 		{
 			echo $i++, '. ', $pls->id, '; cars: ' , $pls->carmakes, '<br>';
+			//посылка прайса
+			echo (PHPMailer( 'temp.xls')) ? 'Mail sent with attachet file ' . $filename  : "Mail failed";
 		}
-	}
-	public function actionSendPriceAttachment()
-	{ 
-		//define the receiver of the email 
-		$to = 'igor.savinkin@gmail.com'; 
-		//define the subject of the email 
-		$subject = 'Test email with attachment'; 
-		//create a boundary string. It must be unique 
-		//so we use the MD5 algorithm to generate a random hash 
-		$random_hash = md5(date('r', time())); 
-		//define the headers we want passed. Note that they are separated with \r\n 
-		$headers = "From: webmaster@example.com\r\nReply-To: webmaster@example.com"; 
-		//add boundary string and mime type specification 
-		$headers .= "\r\nContent-Type: multipart/mixed; boundary=\"PHP-mixed-".$random_hash."\""; 
-		//read the atachment file contents into a string,
-		//encode it with MIME base64,
-		//and split it into smaller chunks
-		$attachment = chunk_split(base64_encode(file_get_contents(Yii::app()->basePath.'/files/tempSp.xlsx'))); 
-		//define the body of the message. 
-		ob_start(); //Turn on output buffering 
-		?> 
-		--PHP-mixed-<?php echo $random_hash; ?>  
-		Content-Type: multipart/alternative; boundary="PHP-alt-<?php echo $random_hash; ?>" 
-
-		--PHP-alt-<?php echo $random_hash; ?>  
-		Content-Type: text/plain; charset="iso-8859-1" 
-		Content-Transfer-Encoding: 7bit
-
-		Hello World!!! 
-		This is simple text email message. 
-
-		--PHP-alt-<?php echo $random_hash; ?>  
-		Content-Type: text/html; charset="iso-8859-1" 
-		Content-Transfer-Encoding: 7bit
-
-		<h2>Hello World!</h2> 
-		<p>This is something with <b>HTML</b> formatting.</p> 
-
-		--PHP-alt-<?php echo $random_hash; ?>-- 
-
-		--PHP-mixed-<?php echo $random_hash; ?>  
-		Content-Type: application/vnd.ms-excel; name="temp.xlsx"  
-		Content-Transfer-Encoding: base64  
-		Content-Disposition: attachment  
-
-		<?php echo $attachment; ?> 
-		--PHP-mixed-<?php echo $random_hash; ?>-- 
-
-		<?php 
-		//copy current buffer contents into $message variable and delete current output buffer 
-		$message = ob_get_clean(); 
-		//send the email 
-		$mail_sent = @mail( $to, $subject, $message, $headers ); 
-		//if the message is sent successfully print "Mail sent". Otherwise print "Mail failed" 
-		echo $mail_sent ? "Mail sent" : "Mail failed";  
-	}
-	public function actionTestPHPMailer()
+	} 
+	public function PHPMailer($filename=null)
 	{
 		Yii::import('ext.phpmailer.JPhpMailer');
 		$mail = new JPhpMailer;
@@ -215,10 +162,11 @@ class PriceListSettingController extends Controller
 		$mail->AltBody = 'To view the message, please use an HTML compatible email viewer!';
 		$mail->MsgHTML('<h3>See the Price List in Attachment !</h3>');
 		$mail->AddAddress('igor.savinkin@gmail.com', 'from Igor Savinkin');
-		$file_to_attach = Yii::app()->basePath.'/../files/tempSp.xlsx';
-		echo $file_to_attach, '<br>';
-		$mail->AddAttachment( $file_to_attach , 'tempSp.xlsx' ); 
+		$filename = 'temp.xls';
+		$filepath = Yii::app()->basePath . '/../files/' . $filename;
+	//	echo $filepath, '<br>';
+		$mail->AddAttachment( $filepath , 'TAРЕКС прайс '. date('d.mY')); 
 		
-		echo $mail->Send() ? "Mail sent with attachet file tempSp.xlsx" : "Mail failed"; 
+		return $mail->Send() ? 1 : 0; //'Mail sent with attachet file ' . $filename  : "Mail failed"; 
 	}
 }
